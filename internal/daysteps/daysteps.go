6b25f11888c3471fc7f8604e50d53906f10244a1
efimovauliana <ulianaefimova@Noutbuk-Uliana.local>
2026-02-05 19:49:44 -0800

    first commit

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -2,6 +2,10 @@ package daysteps
 
 import (
 	"time"
+	"strconv"
+	"strings"
+	"fmt"
+	"github.com/Yandex-Practicum/tracker/internal/spentcalories"
 )
 
 const (
@@ -12,9 +16,68 @@ const (
 )
 
 func parsePackage(data string) (int, time.Duration, error) {
-	// TODO: реализовать функцию
+	// Разделить строку на слайс строк
+	parts := strings.Split(data, ",")
+	
+	// Проверить, чтобы длина слайса была равна 2
+	if len(parts) != 2 {
+		return 0, 0, fmt.Errorf("неправильный формат данных: ожидается 'шаги,время'")
+	}
+
+	stepsStr := strings.TrimSpace(parts[0])
+	durationStr := strings.TrimSpace(parts[1])
+
+	// Преобразовать первый элемент слайса в тип int
+	steps, err := strconv.Atoi(stepsStr)
+	if err != nil {
+		return 0, 0, fmt.Errorf("ошибка преобразования шагов: %v", err)
+	}
+
+	// Проверить: количество шагов должно быть больше 0
+	if steps <= 0 {
+		return 0, 0, fmt.Errorf("количество шагов должно быть больше 0")
+	}
+
+	// Преобразовать второй элемент слайса в time.Duration
+	duration, err := time.ParseDuration(durationStr)
+	if err != nil {
+		return 0, 0, fmt.Errorf("ошибка преобразования времени: %v", err)
+	}
+
+	if duration <= 0 {
+		return 0, 0, fmt.Errorf("продолжительность должна быть положительной")
+	}
+	// Возврат количества шагов, продолжительности и nil
+	return steps, duration, nil
 }
 
 func DayActionInfo(data string, weight, height float64) string {
-	// TODO: реализовать функцию
+	// Получить данные о количестве шагов и продолжительности прогулки
+	steps, duration, err := parsePackage(data)
+	if err != nil {
+		return ""
+	}
+
+	// Проверить, чтобы количество шагов было больше 0
+	if steps <= 0 {
+		return ""
+	}
+
+	// Вычислить дистанцию в метрах
+	distanceMeters := float64(steps) * stepLength
+
+	// Перевести дистанцию в километры
+	distanceKm := distanceMeters / mInKm
+
+	// Вычислить количество калорий
+	calories, err := spentcalories.WalkingSpentCalories(steps, weight, height, duration)
+	
+	if err != nil {
+		return ""
+	}
+
+	result := fmt.Sprintf("Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.",
+		steps, distanceKm, calories)
+	
+	return result
 }
