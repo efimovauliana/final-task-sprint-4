67df94faf8416f3f99d2bf391c24b6ca07c9d4c8
efimovauliana <efimovauliana5@gmail.com>
2026-02-08 08:43:54 -0800

    second commit

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -76,7 +76,7 @@ func DayActionInfo(data string, weight, height float64) string {
 		return ""
 	}
 
-	result := fmt.Sprintf("Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.",
+	result := fmt.Sprintf("Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.\n",
 		steps, distanceKm, calories)
 	
 	return result
